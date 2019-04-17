56342b14ef3409f6d36d7097f716c4ac3c94f6c6
John Kueh <yoongfook@gmail.com>
2019-04-17 23:25:01 +1000

    Update logo path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### React components for a SAAS app
+
+[https://featureready.github.io/saas-boilerplate-components/](https://featureready.github.io/saas-boilerplate-components/)
--- src/components/layouts/auth.js
+++ src/components/layouts/auth.js
@@ -7,7 +7,7 @@ const AuthLayout = ({ children }) => (
       <div className="col-md-4 offset-md-4">
         <div className="card border-0 p-4 bg-light">
           <div className="my-3 text-center">
-            <img alt="logo" height="40" src="/full-logo-black.svg" />
+            <img alt="logo" height="40" src="full-logo-black.svg" />
           </div>
           {children}
         </div>
