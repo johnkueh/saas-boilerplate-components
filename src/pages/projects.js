dcd44c21c8d8594a2a038c48afb60c2bdfc28464
John Kueh <yoongfook@gmail.com>
2019-04-24 09:38:49 +1000

    Updated projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,19 +4,28 @@ import Layout from '../layouts/no-side-nav';
 
 const Projects = () => (
   <Layout title="Here's all your projects" caption="Select one to access it's dashboard">
-    <Link className="d-block" to="/dashboard">
-      v2food.com
-    </Link>
-    <Link className="d-block" to="/dashboard">
-      johnkueh.com
-    </Link>
-    <Link className="d-block" to="/dashboard">
-      zibbetmarketing.com
-    </Link>
+    <ul class="list-group">
+      <li class="list-group-item">
+        <NavLink to="/dashboard">v2food.com</NavLink>
+      </li>
+      <li class="list-group-item">
+        <NavLink to="/dashboard">johnkueh.com</NavLink>
+      </li>
+      <li class="list-group-item">
+        <NavLink to="/dashboard">zibbetmarketing.com</NavLink>
+      </li>
+    </ul>
     <div className="mt-4">
       <span className="text-muted">or</span> <Link to="/projects/new">Create a new project</Link>
     </div>
   </Layout>
+);
+
+const NavLink = ({ to, children }) => (
+  <Link className="d-flex justify-content-between align-items-center" to={to}>
+    <div>{children}</div>
+    <i className="fal fa-chevron-right" />
+  </Link>
 );
 
 export default Projects;
